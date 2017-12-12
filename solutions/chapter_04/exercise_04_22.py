94a8acd9fc94cde6a3ebdc55b2c4a3ba269d56eb
Jendrik Stelzner <jendrikstelzner@web.de>
2017-12-12 19:56:41 +0100

    Added an explanation for exercise 22 of chapter 4.

--- solutions/chapter_04/exercise_04_22.py
+++ solutions/chapter_04/exercise_04_22.py
@@ -7,21 +7,22 @@ def bc(yl, yr): return abs(yl) + abs(yr+2)
 
 I = linspace(0, 4)
 
-y_1 = zeros((1, len(I)))
-y_1[0][0] = 1
-#y_2 = zeros((1, len(I)))
-#y_2[0][1] = -1
+y = zeros((1, len(I)))
+y[0][0] = 1
 
-res_1 = solve_bvp(f, bc, I, y_1)
-#res_2 = solve_bvp(f, bc, I, y_2)
+res = solve_bvp(f, bc, I, y)
 
 x = linspace(0, 4, 100)
-u_1 = res_1.sol(x)[0]
-#u_2 = res_2.sol(x)[0]
+u = res.sol(x)[0]
 
 plt.clf()
-plt.plot(x, u_1, label="u_1")
-#plt.plot(x, u_2, label="u_2")
-plt.plot(x, -0.037*exp(x), label="exp")
+plt.plot(x, u)
+plt.grid()
+plt.show()
+
+plt.clf()
+plt.plot(x, u, "b-", label="u") 
+plt.plot(x, -2*exp(x-4), "r--", label="-2 e^{x-4}")
+plt.grid()
 plt.legend()
 plt.show()
