7dfe97fea1e928862bac96a08ed732dda0322b69
s6jestel <s6jestel@uni-bonn.de>
2018-01-29 14:52:24 +0100

    Tested and corrected the solution to exercise 41.

--- solutions/chapter_08/exercise_08_41.m
+++ solutions/chapter_08/exercise_08_41.m
@@ -1,31 +1,13 @@
-%  untested
-
-%  u'' = -2;
-%  u(0) = 0, u(5) = 3;
-%  
-%  dydx = @(x,y) [y(2) -2]';
-%  bvs = @(lb, rb)[lb(1) rb(1)-3]';
-%  sol_init = bvpinit([0 5], [0 0]);
-%  
-%  sol = bvp4c(dydx, bvs, sol_init);
-%  x = linspace(0,5);
-%  w = deval(sol, x, 1);
-%  u = w(1,:);
-%  
-%  plot(x,u);
-
-
-
 %  u'' - 2 u' + u = 0
 %  u(0) = u(1) = 0
 
-dydx = @(x,y) [y(2), 2 * y(2) + y(1)];
-bvs = @(lb, rb)[lb(1) rb(1)];
-sol_init = bvpinit([0 5] [0 0]);
+dydx = @(x,y) [y(2), 2 * y(2) - y(1)];
+bvs = @(lb, rb)[lb(1), rb(1)]';
+sol_init = bvpinit([0 0.5 1], [0 0]);
 
 sol = bvp4c(dydx, bvs, sol_init);
 x = linspace(0,1);
 w = deval(sol, x, 1);
 u = w(1,:);
 
-plot(x,u);
+plot(x,u); % the zero function
