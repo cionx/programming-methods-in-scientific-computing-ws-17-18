f2e0bcf5b30f5c2ce5549327a54d65f7d1df071d
Jendrik Stelzner <jendrikstelzner@web.de>
2018-01-30 22:58:39 +0100

    Removed old code.

--- solutions/chapter_07/chapter_07_exercise_36.cpp
+++ solutions/chapter_07/chapter_07_exercise_36.cpp
@@ -9,7 +9,7 @@ double f_exact(double x){
 }
 
 int main(){
-  int n = 20;
+  int n = 24;
   
   double left = 0;    // left interval value
   double right = 1;   // right interval value
@@ -20,7 +20,7 @@ int main(){
   std::vector<double> u(n+1,0);     // additional point u[n] to the right of the interval
   u[0] = lb;
   
-  Matrix A = Matrix(n,n);              // LGS for the values u[1] to u[n]
+  Matrix A = Matrix(n,n);           // LGS for the values u[1] to u[n]
   std::vector<double> b(n,0);
   
   A(0,0) = -2/(h*h) + 5/h + 4;
@@ -60,44 +60,4 @@ int main(){
               << std::endl;
     x += h;
   }
-  //   std::cout << std::endl;
-  // // approximation
-  //   for(int i = 0; i < n; i++)
-  //     std::cout << u[i] << "\t| ";
-  //   std::cout << std::endl;
-  // // exact result
-  //   for(int i = 0; i < n; i++)
-  //     std::cout <<  << "\t| ";
-  //   std::cout << std::endl;
-  
-//   Matrix B(5,5,0);
-//   //the matrix:
-//   
-//   B(0,0)=-8;B(0,1)=-4; 
-//   B(1,0)= 16;	B(1,1)=-8;B(1,2)=-4;
-//   B(2,1)= 16;	B(2,2)=-8;B(2,3)=-4;
-//   B(3,2)= 16;	B(3,3)=-8;B(3,4)=-4;
-//   B(4,3)=1;	B(4,4)=-1;
-//   
-//   std::vector<double> v(5),z(5);
-//   
-//   //the vector:
-//   z[0]= .125;
-//   z[1]= .25;
-//   z[2]= 9.0/16.0;
-//   z[3]= 1;
-//   z[4]= 0;
-//   
-//   //solving
-//   v=B.gaussSolve(z);
-//   //print:
-//   std::cout << "Exc. 36:\nvalues of u(x) for x = 0, 0.25, ..., 1:" << "\n";
-//   std::cout << "0 | ";
-//   for (int i=0; i<5; i++)
-//     std::cout << v[i] << " | ";
-//   std::cout << "\n";
-//   for (int i=0; i<5; i++){
-//     std::cout << f_exact(i/4.0) << " | ";
-//   }
-//   std::cout << "\n";
 }
