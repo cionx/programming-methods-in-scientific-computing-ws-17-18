ac02ccd6efd2aea6128c967e08e97eb07a550bc3
Jendrik Stelzner <jendrikstelzner@web.de>
2018-01-30 18:40:14 +0100

    Reworked exercise 35.

--- solutions/chapter_07/chapter_07_exercise_35.cpp
+++ solutions/chapter_07/chapter_07_exercise_35.cpp
@@ -1,38 +1,59 @@
-#include<cmath>
-#include<vector>
-#include<iostream>
+#include "matrix.hpp"
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <iomanip>
 
-#include "matrix.cpp"
+
+
+double f_exact(double x){ return 2*sinh(2*x); }; // the exact solution
 
 int main(){
-	//first ODE (EXC 35):
-	double u0=0;
-	double u4=exp(2)-exp(-2);
-	Matrix A(3,3,0);
-	// double h=.25;
-	double c=2.25;  // = (4*h*h)+2; //auxiliary constant
-	//the matrix:
-	
-	A(0,0)= c; 	A(0,1)=-1; 
-	A(1,0)=-1;	A(1,1)= c;	A(1,2)=-1;
-				A(2,1)=-1;	A(2,2)= c;
-	
-	std::vector<double> u(3),y(3);
-	
-	//the vector:
+  int n = 7;                    // number of points
+  double left = 0;              // left border
+  double right = 1;             // right border
+  double lb = 0;                // left boundary value
+  double rb = exp(2) - exp(-2); // right boundary value
+  
+  std::vector<double> u(n,0);
+  u[0] = lb;
+  u[n-1] = rb;
+  
+  int m = n-2;                      // number of points in the middle
+  double h = (right - left)/(n-1);  // length of intervals
+  double C = 2 + 4*h*h;
+  
+  Matrix A = Matrix(m, m, 0);
+  for(int i = 0; i < m; i++)
+    A(i,i) = C;
+  for(int i = 0; i < m-1; i++){
+    A(i,i+1) = -1;
+    A(i+1,i) = -1;
+  }
+  std::vector<double> b(m,0);
+  b[0] = u[0];
+  b[m-1] = u[n-1];
+  std::vector<double> u_inner = A.gaussSolve(b);
+  for(int i = 0; i < m; i++){
+    u[i+1] = u_inner[i];
+  }
+  
+  std::cout << std::fixed;
+  std::cout << std::setprecision(8);
+  // x values
+  std::cout << "x\t\t| ";
+  for(int i = 0; i < n; i++)
+    std::cout << (left + i*h) << "\t| ";
+  std::cout << std::endl;
+  // approx values
+  std::cout << "approx(x)\t| ";
+  for(int i = 0; i < n; i++)
+    std::cout << u[i] << "\t| ";
+  std::cout << std::endl;
+  // exact values
+  std::cout << "exact(x)\t| ";
+  for(int i = 0; i < n; i++)
+    std::cout << f_exact(left + i*h) << "\t| ";
+  std::cout << std::endl;
 
-	y[0]= u0;
-	y[1]= 0;
-	y[2]= u4;
-	
-	//solving:
-	u=A.gaussSolve(y);
-	//print and compare to exact solution:
-	std::cout << "Exc. 35:\nvalues of u(x) for x = 0, 0.25, ..., 1:" << "\n";
-	std::cout << u0 << " | ";
-	for (int i=0; i<3; i++) std::cout << u[i] << " | ";
-	std::cout << u4 << " | \n";
-	std::cout << "exact solution:" << "\n";
-	for (int i=0; i<5; i++) std::cout << (exp(.5*i)-exp(-.5*i)) << " | ";
-	std::cout << "\n";
 }
