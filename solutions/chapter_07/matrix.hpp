d65466653cf48831d48f21c56606f4ef3eb78f7e
Jendrik Stelzner <jendrikstelzner@web.de>
2018-01-30 16:45:38 +0100

    Reorganized the matrix class.

---
+++ solutions/chapter_07/matrix.hpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+#define EPS 1.E-14
+
+
+class Matrix{
+    std::vector<std::vector<double>> mat;
+    int rows, cols;
+    
+  public:
+    
+    Matrix();
+    Matrix(int i, int j);
+    Matrix(int i, int j, double v);
+    double& operator() (int i, int j);
+    
+    // matrix operations
+    Matrix operator-();
+    Matrix operator+(Matrix B);
+    Matrix operator-(Matrix B);
+    Matrix operator*(Matrix B);
+    
+    //  elementary row operations
+    void permuteRows(int i,  int j);
+    void multiplyRow(int i, double c);
+    void addRowTo(int i,  int j, double c);
+    std::vector<double> gaussSolve(std::vector<double> y);
+    
+//  comparing and printing
+    
+    Matrix clean();
+    bool operator==(Matrix B);
+    void print();
+};
