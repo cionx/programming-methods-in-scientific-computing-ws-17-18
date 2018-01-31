5df23786cc851259ef47c7c34e90a7d09dad09c6
Jendrik Stelzner <jendrikstelzner@web.de>
2018-01-31 01:51:00 +0100

    Added latex code for exercise 39.

--- solutions/chapter_08/exercise_08_39.m
+++ solutions/chapter_08/exercise_08_39.m
@@ -1,13 +1,20 @@
-n = 20;
-x = 0 : n;
-y = x + 2*(rand(size(x)) - 0.5);
+n = 20;     % number of points
+x = 0 : n;  % x values
+y = x + 2*(rand(size(x)) - 1);  % y values
 
-A = zeros(length(x), 2);
-A(:,1) = x';
-A(:,2) = ones(length(y),1);
+A = zeros(length(x), 2);    % initialize zero matrix
+A(:,1) = x';                % insert x values in first column
+A(:,2) = ones(length(y),1); % insert ones into second column
 
-a = (A\y')';
+a = (A\y')';  % find coefficients of regression line
 
-hold on;
-plot(x, y, '*');
-plot(x, a(1)*x + a(2));
+hold on;                % to plot both points and the line 
+plot(x, y, '*');        % plotting the points
+plot(x, a(1)*x + a(2)); % plotting the line
+set(h, 'PaperUnits','centimeters');
+set(h, 'Units','centimeters');
+pos=get(h,'Position');
+set(h, 'PaperSize', [pos(3) pos(4)]);
+set(h, 'PaperPositionMode', 'manual');
+set(h, 'PaperPosition',[0 0 pos(3) pos(4)]);
+print('-dpdf','exercise_08_39_figure_test.pdf')
